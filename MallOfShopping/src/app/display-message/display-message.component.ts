823c8e2951b98c7b74c45dc544f1aa8e63b61637
anishspillai <anishspillai@gmail.com>
2020-06-01 16:34:45 +0200

    Committing

--- MallOfShopping/src/app/display-message/display-message.component.ts
+++ MallOfShopping/src/app/display-message/display-message.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Message} from "primeng/messages";
+import {Message} from "primeng/api";
 
 @Component({
   selector: 'app-display-message',
