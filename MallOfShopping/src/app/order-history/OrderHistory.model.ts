3095faaefaf52355251c769c56ead2af135679b9
anishspillai <anishspillai@gmail.com>
2020-06-09 19:21:21 +0200

    Committing

--- MallOfShopping/src/app/order-history/OrderHistory.model.ts
+++ MallOfShopping/src/app/order-history/OrderHistory.model.ts
@@ -3,9 +3,4 @@ import {Order} from "../individual-grocery/model/Order";
 export class OrderHistoryModel {
   orderedTimestamp: string
   orderHistory: Order[] = []
-
-  public getTotalAmount() {
-    return this.orderHistory
-      .reduce((sum, current) => sum + (current.noOfItems * current.actualPrice), 0);
-  }
 }
